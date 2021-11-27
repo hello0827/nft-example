463747819ecfeb02c9efbe6b2e02054c2711592d
William Mui <william.mui@emali.io>
2021-11-27 18:31:28 +0800

    env update

--- deployContract/src/erc1155.js
+++ deployContract/src/erc1155.js
@@ -1,4 +1,4 @@
-require('dotenv').config({ path: '../test.env' })
+require('dotenv').config({ path: '../demo.env' })
 const Web3 = require("web3");
 let web3 = new Web3(
     // Replace YOUR-PROJECT-ID with a Project ID from your Infura Dashboard
