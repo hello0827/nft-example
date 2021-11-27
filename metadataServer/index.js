96435cd0a2c770c3ebebea08efb453f6fdd3f773
William Mui <william.mui@emali.io>
2021-11-27 19:54:54 +0800

    update for aws app runner

--- metadataServer/index.js
+++ metadataServer/index.js
@@ -1,10 +1,17 @@
-if(process.env.EXPOSE_PORT == undefined){
-    //aws app runner
-    require('dotenv').config({ path: 'aws.env' })
-}
+
 const express = require("express");
 const app = express();
+var port = process.env.EXPOSE_PORT
+if(port == 80){
+    //aws app runner
+    app.use(express.static('metadataServer/public'));
+}else if(port == 8080){
+    //docker
+    app.use(express.static('public'));
+}else{
+    port = 8080;
+    app.use(express.static('public'));
 
-app.use(express.static('public'));
+}
 
 app.listen(process.env.EXPOSE_PORT, () => console.log('Node server listening on port '+process.env.EXPOSE_PORT+'!'));
