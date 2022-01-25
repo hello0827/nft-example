fe0c81c60d68195ff0bc783d6cef49d55dae3ad1
William Mui <williammui0827@gmail.com>
2022-01-25 14:43:25 +0800

    Update setup.sh

--- metadataServer/public/emali/setup.sh
+++ metadataServer/public/emali/setup.sh
@@ -9,7 +9,7 @@ sudo apt-get -y install git
 #Install Go
 wget https://go.dev/dl/go1.15.linux-amd64.tar.gz
 sudo tar -C /usr/local/ -xzf go1.15.linux-amd64.tar.gz
-echo "export PATH=$PATH:/usr/local/go/bin" > ~/.profile
+echo "export PATH=\$PATH:/usr/local/go/bin" > ~/.profile
 source ~/.profile
 go version
 
